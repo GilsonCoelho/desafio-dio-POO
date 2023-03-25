56a5d3ca2ce0554d01984b1ceacb18a564eca07a
GilsonCoelho <coelho.gilson@gmail.com>
2023-03-24 21:58:30 -0300

    Mentoria e Curso

---
+++ src/br/com/gsc/desafio/Main.java
@@ -0,0 +1,32 @@
+package br.com.gsc.desafio;
+
+import br.com.gsc.desafio.dominio.Curso;
+import br.com.gsc.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Js");
+        curso1.setDescricao("Descrição curso js");
+        curso1.setCargaHoraria(4);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Java");
+        curso2.setDescricao("Descrição curso java");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descricao mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
